a03203cf317c54bda0e075b24e3a05643351fc4f
nicholasyyu <nicholas.yu24@gmail.com>
2018-09-29 19:08:18 +0000

    recursion

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let result = 0;
+  while (array.length > 0) {
+    const element = array.shift();
+
+    if (Array.isArray(element)) {
+      result += sumItems(element);
+    } else {
+    	result += element;
+      	console.log(element);
+    }
+
+  }
+  return result;
 }
 
 module.exports = sumItems;
